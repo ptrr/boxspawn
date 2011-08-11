d4c9bc9ad882fa9104b884444c7495d0ac239d38
Peter de Ruijter <peter@l2code.nl>
2011-08-11 09:40:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxspawn::Application.config.secret_token = '0320962bd05de0037a4dd5af2440098367aa8a4d184a8e6c3c40110ebce27acafdb3e7b308b210bba54f49875995faa7546db4b808907be5879ad7e5eab0ae06'
